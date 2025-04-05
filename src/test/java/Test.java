69509ca83a9bf8ea9a17f84537a6c46e80bf25ea
Shyrasya <41370183+Shyrasya@users.noreply.github.com>
2025-04-05 20:01:41 +0300

    init commit

--- src/test/java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    //  Для тестов для файлов:
-//    InputStream inputStream = getClass().getClassLoader().getResourceAsStream("test-flights.txt");
-//    if (inputStream != null) {
-//        filtersHolder.getFlightsFromFile(inputStream);
-//    }
-}
